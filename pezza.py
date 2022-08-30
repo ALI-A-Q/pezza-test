d7885c50dab153fb0c4f3ef9c2f0d2851003f912
ALI-A-Q <i-k-a4@hotmail.com>
2022-08-31 00:01:19 +0300

    nothing

---
+++ pezza.py
@@ -0,0 +1,16 @@
+pezza=str('thick'or 'Superfine')
+
+Kinds_of_pizza = str("Pepperoni"or "Margherita"or "Margherita",)
+
+#def pezz_type(thick,Superfine):
+print(input("plese enter you shose yoy pezza type"))
+
+if pezza == ("thick")or pezza==('Superfine'):
+
+ print(input('you shose you pezza'))
+
+if Kinds_of_pizza ==("Pepperoni")or Kinds_of_pizza ==('Margherita')or Kinds_of_pizza==('Margherita'):
+
+ print("you pezza is rady now ",pezza, Kinds_of_pizza)
+
+ print('thanks for coming (:')
